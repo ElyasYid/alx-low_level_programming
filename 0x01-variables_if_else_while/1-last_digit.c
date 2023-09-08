9c052a5e1bdd3851d5cc5937c60cc2ccc3796e6b
ElyasYid <elyassayle27@gmail.com>
2023-09-08 03:26:53 -0700

    jkalo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,10 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
- * main = Entry point
- * Return: Always return 0 (Successful)
-*/
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
