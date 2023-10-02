41e5c0cc4c8399f363a66fd3a8322ae11eb8584e
ElyasYid <elyassayle27@gmail.com>
2023-10-02 05:18:45 -0700

    uouo

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of program
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (success)
+ */
+
+int main (int argc, char *argv[]) {
+	argc++;
+	printf("%s\n", argv[0]);
+	return 0;
+}
