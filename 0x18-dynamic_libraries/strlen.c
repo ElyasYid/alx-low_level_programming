792131111cc775dd3f1cb3106a6981f5604a2784
ElyasYid <elyassayle27@gmail.com>
2023-12-11 02:07:24 -0800

    kaaa

---
+++ 0x18-dynamic_libraries/strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int r;
+
+	r = 0;
+
+	while (s[r] != '\0')
+	{
+		r++;
+	}
+
+	return (r);
+}
